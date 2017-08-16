2ef09c48fb8794f5a311b67df8ed25c38a0fc002
VaneGarciaYara <vanegarciay@gmail.com>
2017-08-16 12:03:41 -0300

    archivos iniciales

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # tastyFood
 Aplicación web mobile first, TastyFood, donde el cliente puede registrarse, buscar restaurantes por ciudad, muestra un mapa con los tipos de cocina y guarda en favoritos.
+
+**Reto TALENT FEST**
+
+**Requerimientos Técnicos**
+
+El proyecto fue realizado con las siguientes dependencias NPM:
+
+**Dependencias de desarrollo**
+
+    - gulp ~v.3.9.1
+    - gulp-Concat ~v.2.6.1
+    - gulp-Jshint ~v.2.04
+    - gulp-minify-css ~v.1.2.4
+    - gulp-sass ~v.3.0.0
+    - gulp-watch ~4.3.11
+    - gulp-Uglify ~v.2.0.0
+    - gulp-Webserver ~0.9.1
+
+**Dependencias de Producción**
+
+    - materialize: ^1.0.0
+    - jquery ~v.3.1.1
+
+**Clonar repositorio**
+
+    * Posicionado en nuestro proyecto hacer corre npm install para descargar e instalar todas las dependencias utilizadas. 
+    * Correr gulp para que se realizen las tareas especificadas.
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require ('gulp');
+var concat = require ('gulp-concat');
+var uglify = require ('gulp-sass');
+var sass = require ('gulp-sass');
+var minifyCSS = require ('gulp-minify-css');
+var webserver = require('gulp-webserver');
+
+gulp.task('script', function(){
+	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/materialize-css/dist/js/materialize.js', 'assets/js/*.js'])
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('style', function(){
+	gulp.src(['node_modules/materialize-css/dist/css/materialize.css', 'assets/sass/main.scss'])
+	.pipe(sass().on('error', sass.logError))
+	.pipe(minifyCSS())
+	.pipe(concat('style.min.css'))
+	.pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('assets/sass/main.scss', ['style']);
+});
+
+gulp.task('webserver', function(){
+	gulp.src('../tastyFood/')
+		.pipe(webserver({
+			fallback: 'index.html',
+			livereload: true,
+			directoryListing: false,
+			open:true,
+			port: 8080
+		}));
+});
+
+gulp.task('default', ['script', 'style', 'watch','webserver']);
