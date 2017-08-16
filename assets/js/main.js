7bc055a2d8d546edee737e77294cd6c9b1d36ae6
Paulina Rojas <lindaliel@gmail.com>
2017-08-16 18:55:17 -0300

    modifico index, signup y creo login

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function() {  //inicializar navbar de materialize
     $(".button-collapse").sideNav();
+
+    /*validación de formulario*/
+	var correo = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+	var password = /^[0-9]+(\.[0-9])?$/;
+
+
 });
