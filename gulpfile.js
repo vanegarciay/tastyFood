0ac9cce07b15e52f8cbd2e1efecf4cb5aeac5a34
camila-frangp <camila.frangp@gmail.com>
2017-08-18 00:45:26 -0300

    Unión y correción de estilos de html search

--- gulpfile.js
+++ gulpfile.js
@@ -23,4 +23,15 @@ gulp.task('watch', function(){
 	gulp.watch('assets/sass/main.scss', ['style']);
 });
 
-gulp.task('default', ['script', 'style', 'watch']);
+gulp.task('webserver', function(){
+	gulp.src('../tastyFood/')
+		.pipe(webserver({
+			fallback: 'index.html',
+			livereload: true,
+			directoryListing: false,
+			open:true,
+			port: 8080
+		}));
+});
+
+gulp.task('default', ['script', 'style', 'watch','webserver']);
